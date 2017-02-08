4ffde398728698fd21bfc625af884fc497f16578
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2017-02-08 21:01:02 -0200

    Add files via upload

---
+++ Contador/Contador.ino
@@ -0,0 +1,31 @@
+#include <TM1637Display.h>
+#include <Arduino.h>
+
+TM1637Display display(6, 7); //Conexões com a tela (CLK, DIO)
+
+volatile byte rpmcount;
+int valor_recebido;
+
+void rpm_fun()
+{
+  rpmcount++;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  attachInterrupt(0, rpm_fun, FALLING);
+}
+void tacometro()
+{
+  display.showNumberDec (rpmcount, true);
+  Serial.println(rpmcount);
+}
+
+void loop() {
+  display.setBrightness(0x0f);
+  tacometro();
+}
+
+
+
